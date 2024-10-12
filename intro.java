bb509a52ee6ba84f7c78d277e514c32b9ae9d585
Kiash254 <codewithkiash@gmail.com>
2024-10-12 17:26:12 +0300

    adde intro code

---
+++ intro.java
@@ -0,0 +1,11 @@
+// create a hello world in java and also show how to run the code
+//
+
+public class intro {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Kiash on Java");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+    }
+}
